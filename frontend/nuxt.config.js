2b4b8154f42c404e9a3ebb864d708bebdbeb2bcd
Ivan Vinski <ivan.vinski98@gmail.com>
2023-05-16 16:15:58 +0200

    Add nuxtjs/pwa plugin

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -34,6 +34,7 @@ export default {
     // https://go.nuxtjs.dev/axios
     "@nuxtjs/axios",
     "@nuxtjs/i18n",
+    "@nuxtjs/pwa",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
